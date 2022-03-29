282a85a6e49e0250f62b5a29a8c32513cac7cf95
laszlo-ratesic <keenanrc@hotmail.com>
2022-03-29 08:58:11 -0500

    reconfigure sidenav

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,15 +1,23 @@
-const router = require('express').Router();
+const router = require("express").Router();
 
-router.get('/', (req, res) => {
-    res.render('dashboard');
-})
+router.get("/", (req, res) => {
+  res.render("dashboard");
+});
 
-router.get('/profile', (req, res) => {
-    res.render('profile');
-})
+router.get("/profile", (req, res) => {
+  res.render("profile");
+});
 
-router.get('/settings', (req, res) => {
-    res.render('settings');
-})
+router.get("/settings", (req, res) => {
+  res.render("settings");
+});
+
+router.get("/billing", (req, res) => {
+  res.render("billing");
+});
+
+router.get("/pricing", (req, res) => {
+  res.render("pricing");
+});
 
 module.exports = router;
