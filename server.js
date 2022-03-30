0e34d7fbce2ff63f5e06818468bef4e4c807013b
laszlo-ratesic <keenanrc@hotmail.com>
2022-03-30 10:59:35 -0500

    add utils

--- server.js
+++ server.js
@@ -4,7 +4,8 @@ const routes = require("./controllers");
 const sequelize = require("./config/connection");
 const path = require("path");
 const exphbs = require("express-handlebars");
-const hbs = exphbs.create();
+const helpers = require('./utils/helpers');
+const hbs = exphbs.create({ helpers });
 
 const app = express();
 const PORT = process.env.PORT || 5000;
