92d93c73e49d6755671be6e44ae0e6ef0d583769
laszlo-ratesic <keenanrc@hotmail.com>
2022-03-31 08:09:15 -0500

    update wilson margin spacing

--- public/js/scroll.js
+++ public/js/scroll.js
@@ -10,12 +10,12 @@ $(window).on('load', function () {
         if (objectBottom < windowBottom) {
           //object comes into view (scrolling down)
           if ($(this).css('opacity') == 0) {
-            $(this).fadeTo(500, 1);
+            $(this).fadeTo(350, 1);
           }
         } else {
           //object goes out of view (scrolling up)
           if ($(this).css('opacity') == 1) {
-            $(this).fadeTo(500, 0);
+            $(this).fadeTo(350, 0);
           }
         }
       });
